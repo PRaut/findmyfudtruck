a45d3c2c837437f67f9948f0fa6830c36c09acda
Prathamesh Raut <prathamesh.raut001@gmail.com>
2020-05-12 19:03:57 +0530

    made responsive

--- js/custom.js
+++ js/custom.js
@@ -10,4 +10,12 @@ $(document).ready(function(){
       });
     }
   });
+  $(window).scroll(function() {
+    if ($(this).scrollTop() >20){  
+        $('header').addClass("sticky");
+      }
+      else{
+        $('header').removeClass("sticky");
+      }
+    });
 });
