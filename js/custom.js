c88bff284f4fcdccfbf4bac819553cb777837f76
Prathamesh Raut <prathamesh.raut001@gmail.com>
2020-05-19 19:08:15 +0530

    header change

--- js/custom.js
+++ js/custom.js
@@ -1,21 +1,28 @@
 $(document).ready(function(){
-  $(".navigation li a").on('click', function(event) {
-      if (this.hash !== "") {
-      event.preventDefault();
-      var hash = this.hash;
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function(){
-      window.location.hash = hash;
-      });
-    }
+ // https://twitter.com/uixmat
+
+function scrollNav() {
+  $('.navigation li a').click(function(){
+    $('html, body').stop().animate({
+      scrollTop: $($(this).attr('href')).offset().top - 150
+    }, 300);
+    return false;
   });
+}
+scrollNav();
   $(window).scroll(function() {
-    if ($(this).scrollTop() >20){  
+    if ($(this).scrollTop() >0){  
         $('header').addClass("sticky");
       }
       else{
         $('header').removeClass("sticky");
       }
     });
+   $.fn.delay = function(time, callback){
+      jQuery.fx.step.delay = function(){};
+      return this.animate({delay:1}, time, callback);
+    }
+     
+    $("#loader").delay(2500).fadeOut("slow");
 });
+
